ed261af88a1d726f6baed0b76041c1eba1be4597
progamax <maxenceb1207@gmail.com>
2023-03-07 17:47:28 +0100

    Ajout d'un geste pour zoomer la grille d'aide

--- lib/painters/circular_cut.dart
+++ lib/painters/circular_cut.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 
 class CircularCut extends CustomPainter {
   int numberParts;
+  double scale;
 
-  CircularCut(this.numberParts);
+  CircularCut(this.numberParts, this.scale);
 
   @override
   void paint(Canvas canvas, Size size) {
+    canvas.clipRect(Offset.zero & size);
     //Centrer le point d'origine du canvas (il est dans le coin supérieur gauche d'origine)
     canvas.translate(size.width / 2, size.height / 2);
     //Ajout d'un "padding" autour du dessin
@@ -18,14 +20,14 @@ class CircularCut extends CustomPainter {
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke
       ..strokeWidth = 5;
-    canvas.drawCircle(Offset.zero, size.width / 2, paint);
+    canvas.drawCircle(Offset.zero, size.width / 2 * scale, paint);
     for (var i = 0; i < numberParts; i++) {
       canvas.drawLine(
-          Offset.zero, Offset.fromDirection((((i + 1) / numberParts) * 2 * pi) - (1 / 2) * pi, size.width / 2), paint);
+          Offset.zero, Offset.fromDirection((((i + 1) / numberParts) * 2 * pi) - (1 / 2) * pi, size.width / 2 * scale), paint);
     }
     paint.color = Colors.black;
     paint.strokeWidth = 2;
-    canvas.drawCircle(Offset.zero, size.width / 2 + 4.5, paint);
+    canvas.drawCircle(Offset.zero, size.width / 2 * scale + 4.5, paint);
   }
 
   @override
--- lib/widgets/camera_viewer.dart
+++ lib/widgets/camera_viewer.dart
@@ -2,20 +2,53 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_cut_helper/painters/circular_cut.dart';
 
-class CameraViewer extends StatelessWidget {
+class CameraViewer extends StatefulWidget {
   final CameraController controller;
   final int nbParts;
 
   const CameraViewer(this.controller, this.nbParts, {super.key});
 
   @override
+  State<CameraViewer> createState() => _CameraViewerState();
+}
+
+class _CameraViewerState extends State<CameraViewer> {
+  double helperScale = 1;
+  double pinchScale = 1;
+  bool isScaling = false;
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
       fit: StackFit.expand,
       children: [
-        CameraPreview(controller),
-        Center(child: CustomPaint(painter: CircularCut(nbParts), size: Size.infinite,),)
+        CameraPreview(widget.controller),
+        GestureDetector(
+          onScaleStart: onScaleStart,
+          onScaleEnd: onScaleEnd,
+          onScaleUpdate: onScaleUpdate,
+          child: Center(child: CustomPaint(painter: CircularCut(widget.nbParts, helperScale * pinchScale), size: Size.infinite,),),
+        )
       ],
     );
+  }
+
+  void onScaleStart(ScaleStartDetails details) {
+    isScaling = true;
+  }
+
+  void onScaleEnd(ScaleEndDetails details) {
+    isScaling = false;
+    setState(() {
+      helperScale = helperScale * pinchScale;
+      pinchScale = 1;
+    });
+  }
+
+  void onScaleUpdate(ScaleUpdateDetails details) {
+    print(details);
+    setState(() {
+      pinchScale = details.scale;
+    });
   }
 }
