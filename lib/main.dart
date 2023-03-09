b5a45fdaaa00565b454e6394ae708c3dba4106b4
progamax <maxenceb1207@gmail.com>
2023-03-09 11:53:13 +0100

    Changed Style of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_cut_helper/widgets/camera_viewer.dart';
+import 'package:pizza_cut_helper/widgets/helper_chooser.dart';
 import 'package:pizza_cut_helper/widgets/switch_button.dart';
 
 void main() {
@@ -33,55 +34,69 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PizzaCutHelper',
-      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.red),
+      theme: ThemeData(brightness: Brightness.dark, useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Pizza Cutter Helper"),
         ),
-        body: FutureBuilder<CameraController>(
-          future: futureCameraController,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              var controller = snapshot.data!;
-              return Align(
-                  alignment: Alignment.topCenter,
-                  child: Column(children: <Widget>[
-                    Flexible(flex: 4, child: CameraViewer(controller, sliderValue)),
-                    Slider(
-                        value: sliderValue.toDouble(),
-                        onChanged: (change) {
-                          setState(() {
-                            sliderValue = change.toInt();
-                          });
-                        },
-                        min: 2,
-                        max: 20,
-                        divisions: 18,
-                        label: sliderValue.toString()),
-                    Expanded(
-                        flex: 1,
-                        child: Align(
-                            alignment: Alignment.center,
-                            child: InkResponse(
-                                child: LayoutBuilder(
-                              builder: (context, constraints) => SwitchButton(const Icon(Icons.play_arrow),
-                                  const Icon(Icons.pause), constraints.biggest.height * .85, (activated) {
-                                if (activated) {
-                                  controller.resumePreview();
-                                } else {
-                                  controller.pausePreview();
-                                }
-                              }),
-                            )))),
-                  ]));
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
+        body: SafeArea(
+          minimum: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+          child: FutureBuilder<CameraController>(
+            future: futureCameraController,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var controller = snapshot.data!;
+                return Align(
+                    alignment: Alignment.topCenter,
+                    child: Column(children: <Widget>[
+                      Flexible(flex: 4, child: CameraViewer(controller, sliderValue)),
+                      Slider(
+                          value: sliderValue.toDouble(),
+                          onChanged: (change) {
+                            setState(() {
+                              sliderValue = change.toInt();
+                            });
+                          },
+                          min: 2,
+                          max: 20,
+                          divisions: 18,
+                          label: sliderValue.toString()),
+                      Expanded(
+                          flex: 1,
+                          child: Align(
+                              alignment: Alignment.center,
+                              child: InkResponse(
+                                  child: LayoutBuilder(
+                                builder: (context, constraints) => SwitchButton(const Icon(Icons.play_arrow),
+                                    const Icon(Icons.pause), constraints.biggest.height * .85, (activated) {
+                                  if (activated) {
+                                    controller.resumePreview();
+                                  } else {
+                                    controller.pausePreview();
+                                  }
+                                }),
+                              )))),
+                      Align(
+                        alignment: Alignment.center,
+                        child: HelperChooser(const [
+                          Icon(Icons.incomplete_circle),
+                          Icon(Icons.grid_4x4)
+                        ], size: 56, onSelectedIcon: onSelectedHelper,),
+                      )
+                    ]));
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
         ),
       ),
     );
+  }
+
+  void onSelectedHelper(int helperIndex) {
+
   }
 }
